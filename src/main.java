6dd4b43a717e3e416a2e995482fa9843d9f618cb
Jacob Taylor <jacob_taylor90@yahoo.com>
2023-06-12 22:27:04 -0500

    Add method to remove a node at specified index
    This method gives us the ability to remove a specific node
    at the given index. Logic is similar to that of insert. We
    check for our out of bounds issue and return null. If index
    is equal to zero, we return our removeFirst method. If index
    is equal to length - 1, we return our removeLast method. Otherwise,
    we create two node variables, temp and prev, pointing to index
    to be removed and index just before, respectfully. We point prev.next
    to temp.next and then point temp.next to null

--- src/main.java
+++ src/main.java
@@ -1,54 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
         myLinkedList.append(3);
 
-        System.out.println("LL before insert():");
+        System.out.println("LinkedList Before Remove");
         myLinkedList.printList();
 
-        myLinkedList.insert(1, 2);
+        myLinkedList.remove(1);
 
-        System.out.println("\nLL after insert(2) in middle:");
+        System.out.println("LinkedList After Remove");
         myLinkedList.printList();
-
-        myLinkedList.insert(0, 0);
-
-        System.out.println("\nLL after insert(0) at beginning:");
-        myLinkedList.printList();
-
-        myLinkedList.insert(4, 4);
-
-        System.out.println("\nLL after insert(4) at end:");
-        myLinkedList.printList();
-
-
-        /*
-            EXPECTED OUTPUT:
-            ----------------
-            LL before insert():
-            1
-            3
-
-            LL after insert(2) in middle:
-            1
-            2
-            3
-
-            LL after insert(0) at beginning:
-            0
-            1
-            2
-            3
-
-            LL after insert(4) at end:
-            0
-            1
-            2
-            3
-            4
-
-        */
-
-
     }
 }
