f646e355adbf71044debaf59ac6bb7a62758b931
Jacob Taylor <jacob_taylor90@yahoo.com>
2023-06-12 23:03:59 -0500

    Add reverse method to flip the order of our list
    This method is a in-place solution to flip the order
    of the LinkedList. No new list or copy of the list is created.
    Logic for this is to create a temp variable, set head equal
    to tail and tail equal to temp. This flips the order of the first
    and last nodes in the list. Finally, we create an after and before
    variable. As we loop through the list, we essentially just flip
    the order of the pointers for the middle nodes

--- src/main.java
+++ src/main.java
@@ -3,13 +3,10 @@ public class Main {
         LinkedList myLinkedList = new LinkedList(1);
         myLinkedList.append(2);
         myLinkedList.append(3);
+        myLinkedList.append(4);
 
-        System.out.println("LinkedList Before Remove");
-        myLinkedList.printList();
-
-        myLinkedList.remove(1);
-
-        System.out.println("LinkedList After Remove");
+        myLinkedList.reverse();
+        
         myLinkedList.printList();
     }
 }
