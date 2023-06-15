ff45d60749a160c1810c424010db20b5d35e5e54
Jacob Taylor <jacob_taylor90@yahoo.com>
2023-06-14 23:40:20 -0500

    Add RemoveLast method to DoublyLinkedList
    This method give the ability to remove the last node in the list.
    A temp node is created and set to the tail node. Tail is then
    moved to tail.prev, tail.next and temp.prev is set to null. Finally,
    length is descreased by 1 and temp is returned.

--- src/DoublyLinkedList/DoublyLinkedList.java
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -73,7 +73,22 @@ public class DoublyLinkedList {
         }
         length++;
     }
+
     //RemoveLast
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = tail;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
     //Prepend
     //RemoveFirst
     //Get
--- src/DoublyLinkedList/Main.java
+++ src/DoublyLinkedList/Main.java
@@ -5,6 +5,11 @@ public class Main {
         DoublyLinkedList myDLL = new DoublyLinkedList(1);
         myDLL.printList();
         myDLL.append(2);
+        System.out.println("After Append");
+        myDLL.printList();
+
+        System.out.println("After RemoveLast");
+        myDLL.removeLast();
         myDLL.printList();
     }
 }
